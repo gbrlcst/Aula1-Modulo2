f30720dde525d286559eaad8f9945536b819cb7d
Gabriel Costa <gbrlcst148@gmail.com>
2021-10-31 15:19:29 -0300

    Aula 1 do segundo módulo

---
+++ src/implementacao/ImplementarLivro.java
@@ -0,0 +1,27 @@
+package implementacao;
+
+import beans.Livro;
+
+public class ImplementarLivro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Livro exemplar1 = new Livro(); 
+		
+		exemplar1.titulo = "Programação Java";
+		exemplar1.editora = "Atlas Editora";
+		
+		System.out.println("Exemplar1: " + exemplar1.titulo);
+		System.out.println("- Editora: " + exemplar1.editora);
+		
+		Livro exemplar2 = new Livro();
+		exemplar2.titulo = "Database BigData";
+		exemplar2.editora = "Books";
+		
+		System.out.println("Exemplar2: " + exemplar2.titulo);
+		System.out.println("- Editora: " + exemplar2.editora);
+		System.out.println("- Autor: " + exemplar2.autor);
+	}
+
+}
